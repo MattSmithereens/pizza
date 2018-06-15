0319b1765dfea235ac5d7b31ffa2af5bcb5825a5
Matt <mattsmithereens@gmail.com>
2018-06-15 13:52:23 -0700

    input size and array of toppings of ordered pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
-var pizza = function Pizza(size, toppings, 10) {
-  this.size = size;
+// business
+
+var newPizza = function Pizza(size, [], price) {
+  this.size = inputtedSize;
   this.toppings = [];
   this.price = [];
 }
+
+// Pizza.prototype.showPie = function() {
+//   return this.size + ", " + this.price;
+// }
+
+// user interface
+
+$(document).ready(function() {
+  $("form#buttons").submit(function(event){
+      event.preventDefault();
+      var inputtedSize = $("input:radio[name=size]:checked").val();
+      var toppingArray = Array();
+      $("input:checkbox[name=topping]:checked").each(function() {
+    		toppingArray.push($(this).val());
+    	});
+      console.log(toppingArray);
+      console.log(inputtedSize);
+
+  })
+})
