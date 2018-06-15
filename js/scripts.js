2e096ccddb2349b3e36f0395c3b2ef583fa12049
Matt <mattsmithereens@gmail.com>
2018-06-15 09:51:53 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var pizza = function Pizza(size, pepperoni, veganCheese, cheese, price) {
+  this.size = size;
+  this.pepperoni = pepperoni;
+  this.cheese = cheese
+  this.veganCheese = veganCheese;
+  this.price = [];
+}
