3e4b306a9a1ec0b182a4da063de7efd1e8c30ec1
Matt Smith <mattsmithereens@gmail.com>
2018-06-16 10:49:10 -0700

    add third row of ingredients, clean out extraneous pseudo code from js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// business
+// pizza business
 
 function Pizza(size, toppings, price) {
   this.size = size;
@@ -6,37 +6,15 @@ function Pizza(size, toppings, price) {
   this.price = price;
 }
 
-// pseudo code
-// Pizza.prototype.pizzaPrice = function() {
-//   if this.size contains small return 6
-//
-//   else if this.size contains medium return 8
-//
-//   else return 10
-//
-//   return amount of toppings * 2; +
-// }
-
-//
-//   Pizza.prototype.orderedPizza = function() {
-//     return "you ordered a " this.size + " pizza with " + this.toppingArray;
-//   }
-//
-// }
-//  console.log(Pizza.orderedPizza);
-// Pizza.prototype.showPie = function() {
-//   return this.size + ", " + this.price;
-// }
-
-// user interface
+// hot n tasty user interface
 
 $(document).ready(function() {
   $("form#buttons").submit(function(event){
-      event.preventDefault();
+      event.preventDefault()
       var inputtedSize = $("input:radio[name=size]:checked").val();
-      var toppingArray = Array();
+      var toppingArray = Array()
       $("input:checkbox[name=topping]:checked").each(function() {
-    		toppingArray.push($(this).val());
+    		toppingArray.push($(this).val())
       var toppingsPrice = toppingArray.length * 2;
       if (inputtedSize === "Small") {
         var newPizza = new Pizza(inputtedSize, toppingArray, toppingsPrice + 6);
@@ -45,8 +23,8 @@ $(document).ready(function() {
       } else if (inputtedSize === "Large") {
         var newPizza = new Pizza(inputtedSize, toppingArray, toppingsPrice + 14);
       } else {
-        alert("You need to tell us what size AMAZING pizza you'd like!");
-      } console.log(newPizza);
-    });
+        alert("You need to tell us what size AMAZING pizza you'd like!")
+      } console.log(newPizza)
+    })
   })
 })
