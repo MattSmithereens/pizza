34b84b0db16d83744c723028c4f985b8637ff31c
Matt Smith <mattsmithereens@gmail.com>
2018-06-18 22:24:33 -0700

    refactor code and add prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 // pizza business
 
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings, toppingPrice, piePrice) {
   this.size = size;
   this.toppings = toppings;
-  this.price = price;
+  this.toppingPrice = toppingPrice;
+  this.piePrice = piePrice;
 }
+
+Pizza.prototype.toppingCost = function() {
+  this.toppingPrice = this.toppings.length * 2
+}
+
+Pizza.prototype.pieCost = function() {
+  if (this.size === "Small") {
+    this.piePrice = 6
+  } else if (this.size === "Medium") {
+    this.piePrice = 10
+  } else if (this.size === "Large") {
+    this.piePrice = 14
+  } else {
+    alert("You need to tell us what size AMAZING pizza you'd like!")
+  }
+}
+
+
 
 // hot n tasty user interface
 
@@ -13,20 +32,14 @@ $(document).ready(function() {
     event.preventDefault()
     var inputtedSize = $("input:radio[name=size]:checked").val();
     var toppingArray = Array()
+    var newPizza = new Pizza(inputtedSize, toppingArray);
     $("input:checkbox[name=topping]:checked").each(function() {
-  		toppingArray.push($(this).val())
-    var toppingsPrice = toppingArray.length * 2;
-    if (inputtedSize === "Small") {
-      var newPizza = new Pizza(inputtedSize, toppingArray, toppingsPrice + 6);
-    } else if (inputtedSize === "Medium") {
-      var newPizza = new Pizza(inputtedSize, toppingArray, toppingsPrice + 10);
-    } else if (inputtedSize === "Large") {
-      var newPizza = new Pizza(inputtedSize, toppingArray, toppingsPrice + 14);
-    } else {
-      alert("You need to tell us what size AMAZING pizza you'd like!")
-    }
-      $(".receipt").show();
-      $(".output").text("Your " + newPizza.size + " pizza with " + newPizza.toppings + " will be $" + newPizza.price + ".");
+  	toppingArray.push($(this).val())
+    newPizza.toppingCost();
+    newPizza.pieCost();
+    var totalCharge = parseInt(newPizza.toppingPrice) + parseInt(newPizza.piePrice)
+    $(".receipt").show();
+    $(".output").text("Your " + newPizza.size + " pizza with " + newPizza.toppings + " will be $" + totalCharge + ".");
     })
   })
 })
